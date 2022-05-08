8518d4fad3cf100da4ee42ff2b51677dbef8a029
Justin Ahinon <justiny.ahinon@gmail.com>
2022-05-08 08:54:15 +0200

    Create single post layout

--- src/assets/scripts/queries.js
+++ src/assets/scripts/queries.js
@@ -25,4 +25,39 @@ query blogIndexQuery {
   }
 }`;
 
-export { blogIndexQuery, siteMetaQuery };
+const blogIndexSlugsQuery = `
+query blogIndexSlugsQuery {
+  allPosts {
+    id
+    __typename
+    slug
+  }
+}`;
+
+const singlePostQuery = `
+query singlePostQuery($slug: String!) {
+  post(filter: {slug: {eq: $slug}}) {
+    id
+    __typename
+    _firstPublishedAt
+    title
+    slug
+    content {
+      value
+    }
+    seoMetatags {
+      __typename
+      title
+      description
+      image {
+        id
+        __typename
+        url
+        alt
+        title
+      }
+    }
+  }
+}`;
+
+export { blogIndexQuery, blogIndexSlugsQuery, singlePostQuery, siteMetaQuery };
--- src/assets/scripts/utils.js
+++ src/assets/scripts/utils.js
@@ -1,6 +1,32 @@
+// Externals
+import { render } from "datocms-structured-text-to-html-string";
+
 const readableDate = (date) => {
   const options = { year: "numeric", month: "long", day: "numeric" };
   return new Date(date).toLocaleDateString(undefined, options);
 };
 
-export { readableDate };
+const renderStructuredText = (value) => {
+  const options = {
+    renderBlock({ record, adapter: { renderNode } }) {
+      switch (record.__typename) {
+        case "BlockImageRecord":
+          return renderNode(
+            "figure",
+            {},
+            renderNode("img", {
+              src: record.image.url,
+              alt: record.image.alt,
+              loading: "lazy",
+            })
+          );
+        default:
+          return null;
+      }
+    },
+  };
+
+  return render(value, options);
+};
+
+export { readableDate, renderStructuredText };
