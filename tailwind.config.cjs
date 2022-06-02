d1a4374c16985fc22948f5c6f255c4ddf2d2c95a
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-02 06:18:47 +0200

    Update site footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       primary: "#B37F04",
       "primary-200": "#FDE8B4",
       secondary: "#3772FF",
-      "secondary-100": "#F5F8FF",
+      "secondary-100": "hsl(222deg 25% 80%)",
       white: "#FFFFFF",
       "gray-500": "#6B7280",
       "gray-700": "#374151",
