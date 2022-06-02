00ad29d9b36743a8a6597e77a605a35d1247e402
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-02 07:34:35 +0200

    Update utils.js

--- src/assets/scripts/utils.js
+++ src/assets/scripts/utils.js
@@ -1,32 +1,11 @@
-// Externals
-import { render } from "datocms-structured-text-to-html-string";
+const generateSlug = (content) => {
+  const slug = content.toLowerCase().trim();
+  return slug.replace(/[^a-z0-9]+/g, "-");
+}
 
 const readableDate = (date) => {
   const options = { year: "numeric", month: "long", day: "numeric" };
   return new Date(date).toLocaleDateString(undefined, options);
 };
 
-const renderStructuredText = (value) => {
-  const options = {
-    renderBlock({ record, adapter: { renderNode } }) {
-      switch (record.__typename) {
-        case "BlockImageRecord":
-          return renderNode(
-            "figure",
-            {},
-            renderNode("img", {
-              src: record.image.url,
-              alt: record.image.alt,
-              loading: "lazy",
-            })
-          );
-        default:
-          return null;
-      }
-    },
-  };
-
-  return render(value, options);
-};
-
-export { readableDate, renderStructuredText };
+export { generateSlug ,readableDate };
