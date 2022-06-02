174d72449f1b4ba526ff9b66003a9353dcb83bf8
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-02 09:28:51 +0200

    Remove site from vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://okupter.vercel.app",
+  site: "https://okupter.netlify.app",
   integrations: [
     tailwind({
       config: {
