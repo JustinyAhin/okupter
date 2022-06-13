0e20e4de19c22658f5652cf52ae7529824be748c
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-13 10:24:04 +0200

    Create site.ts

---
+++ src/assets/scripts/site.ts
@@ -0,0 +1,9 @@
+const site = {
+  name: "Okupter",
+  slogan: "On web, performance and accessibility.",
+  publicUrl: "https://www.okupter.com/",
+  ogImage:
+    "https://res.cloudinary.com/segbedji/image/upload/v1654865979/okupter/og-image-okupter_v88rvu.png",
+};
+
+export { site };
